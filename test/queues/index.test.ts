b5b3156c71c91c592a582c620b52dc5828e8f0f7
Charles Roth <charlieroth4@icloud.com>
2021-03-21 11:24:02 -0400

    Queue tested and covered

--- test/queues/index.test.ts
+++ test/queues/index.test.ts
@@ -1,9 +1,35 @@
 import tap from 'tap'
 import { Queue } from '../../src/queues/';
 
-tap.test('Can enqueue a element onto stack', (t) => {
+tap.test('Can enqueue element', (t) => {
   const q = new Queue<number>(5); 
   q.enqueue(1);
   t.equal(q.dequeue(), 1);
   t.done();
 });
+
+tap.test('Cannot enqueue element into full queue', (t) => {
+  const q = new Queue<number>(3); 
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  const extraEnqueueResult = q.enqueue(4);
+  t.equal(extraEnqueueResult, null);
+  t.done();
+});
+
+tap.test('Can dequeue element', (t) => {
+  const q = new Queue<number>(5); 
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  t.equal(q.dequeue(), 1);
+  t.done();
+});
+
+tap.test('Cannot dequeue element from empty queue', (t) => {
+  const q = new Queue<number>(3); 
+  t.equal(q.dequeue(), null);
+  t.done();
+});
+
